6d9499d59ba0ed7976ad41bb72cd222ca05f55da
Erich Schulz <erichbschulz@gmail.com>
2026-02-17 12:01:00 +1000

    simplify

--- src/dwash20260217/__init__.py
+++
@@ -1,7 +0,0 @@
-"""dwash20260217 format benchmark visualization tools."""
-
-from .models import BenchmarkData, ModelResult
-from .reader import read_dwash20260217_file
-from .plotter import create_plot
-
-__all__ = ["BenchmarkData", "ModelResult", "read_dwash20260217_file", "create_plot"]
